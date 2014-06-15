b53a53c194a650434f71c6ca79fa40310ada3240
mariusz <mariusz@aetas.pl>
2014-06-15 18:06:23 +0100

    Unique index added to mongo script for owner_id,name of tag

--- src/main/resources/mongo_scripts/CreateIndices.js
+++ src/main/resources/mongo_scripts/CreateIndices.js
@@ -1,3 +1,9 @@
 var db = connect("localhost:27017/gtweb");
 
-db.sessions.ensureIndex({"last_accessed_time": 1}, {expireAfterSeconds: 1800});
+// TTL for session to expire after given time
+db.sessions.dropIndex({ "last_accessed_time": 1 });
+db.sessions.ensureIndex({"last_accessed_time": 1}, {expireAfterSeconds: 3600});
+
+// Uniqueness of the owner_id,name in the tags
+db.tags.dropIndex  ({"owner_id": 1, "name": 1});
+db.tags.ensureIndex({"owner_id": 1, "name": 1}, {unique: 1});
