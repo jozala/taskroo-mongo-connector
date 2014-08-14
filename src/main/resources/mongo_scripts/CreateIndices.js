23d4435f0d3d210443f09fabb43bc3b5e07219af
mariusz <mariusz@aetas.pl>
2014-08-14 01:27:03 +0100

    Remove rememberMeToken from DB after 2 weeks added to mongo script

--- src/main/resources/mongo_scripts/CreateIndices.js
+++ src/main/resources/mongo_scripts/CreateIndices.js
@@ -4,6 +4,10 @@ var db = connect("localhost:27017/taskroo");
 db.sessions.dropIndex({ "last_accessed_time": 1 });
 db.sessions.ensureIndex({"last_accessed_time": 1}, {expireAfterSeconds: 3600});
 
+// TTL for rememberMeToken to expire after given time
+db.rememberMeTokens.dropIndex({ "create_time": 1 });
+db.rememberMeTokens.ensureIndex({"create_time": 1}, {expireAfterSeconds: 1209600});
+
 // Uniqueness of the owner_id,name in the tags
 db.tags.dropIndex  ({"owner_id": 1, "name": 1});
 db.tags.ensureIndex({"owner_id": 1, "name": 1}, {unique: 1});
